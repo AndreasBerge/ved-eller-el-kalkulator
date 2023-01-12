e09ddbf22df68e058e42c1e9a909360ca4fe129f
Andreas Berge <Andreas Berge>
2023-01-12 22:08:50 +0100

    added api for el prices

--- backup/app.js
+++ backup/app.js
@@ -12,8 +12,6 @@ const resDiv = document.querySelector("#resDiv");
 const res0 = document.querySelector("#res0");
 const res1 = document.querySelector("#res1");
 
-const select = document.querySelector("select");
-
 function calculator(vedPris, virkningsGrad, spotPris) {
     // console.log(vedPris, virkningsGrad, spotPris)
     const virkningsGradFac = virkningsGrad / 100;
@@ -21,40 +19,16 @@ function calculator(vedPris, virkningsGrad, spotPris) {
     const elPris = (netEffect * spotPris) / 100;
     if (Number(elPris) === Number(vedPris)) { res1.style.display = "none"; return res0.textContent = "Lik pris!" }
     else if (Number(elPris) > Number(vedPris)) {
-        const difference = (elPris - vedPris).toFixed(1);
+        const difference = elPris - vedPris;
         res0.textContent = `I dette tilfellet er det rimeligere å fyre med ved!`;
         res1.textContent = `Man sparer ${difference.toLocaleString()} kr per sekk.`;
     } else {
-        const difference = (vedPris - elPris.toFixed(1));
+        const difference = vedPris - elPris;
         res0.textContent = "I dette tilfellet er elektrisk oppvarming rimeligere!";
         res1.textContent = `Ved er ${difference.toLocaleString()} kr dyrere per sekk.`;
     }
     resetButton.style.display = "inline";
     resDiv.classList.add("resDivAfter");
-}
-
-async function selectArea(area) {
-    const dateNow = new Date;
-    const year = dateNow.getFullYear();
-    let month = dateNow.getMonth() + 1;
-    let day = dateNow.toLocaleDateString().split(".")[0];
-    if (day.length === 1) {
-        day = "0" + day;
-    };
-    if (month.toString().length === 1) {
-        month = "0" + month;
-    };
-    const strømPris = await axios.get(`https://www.hvakosterstrommen.no/api/v1/prices/${year}/${month}-${day}_${area}.json`);
-    for (let pris of strømPris.data) {
-        let timeNow = new Date(pris.time_start);
-        if (timeNow.getHours() === dateNow.getHours()) {
-            if (area === "NO4") {
-                inp3.value = (pris.NOK_per_kWh * 100).toFixed(2);
-            } else {
-                inp3.value = (pris.NOK_per_kWh * 100 * 1.25).toFixed(2);
-            };
-        };
-    };
 }
 
 function resetValues() {
@@ -66,17 +40,11 @@ function resetValues() {
     inp1.value = null;
     inp2.value = null;
     inp3.value = null;
-    select.value = "";
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     calculator(inp1.value, inp2.value, inp3.value);
-})
-
-select.addEventListener("change", () => {
-    if (!select.value) { inp3.value = null; }
-    else selectArea(select.value);
 })
 
 resetButton.addEventListener("click", (e) => {
